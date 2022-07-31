5f9ea865289151b113b0cb908b2af38a554482f0
Chris Lajoie <chris@ettaviation.com>
2022-07-31 16:53:49 -0600

    Support VS2022

--- Constants.cs
+++ Constants.cs
@@ -4,7 +4,7 @@ namespace ChrisLajoie.DupSelection
 {
     static class Constants
     {
-        public const string VERSION = "2.2";
+        public const string VERSION = "2.3";
 
         public const string guidDupSelectionPkgString = "e5f7e157-f686-46b7-a588-85b08cdaa5f0";
         public const string guidDupSelectionCmdSetString = "85dcd5f2-19a5-4ee2-a99b-4fac4dc5c4ca";
